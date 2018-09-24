1760175313734f204967c86f000f69d9ca00af1a
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-09-24 14:26:53 -0700

    adding kickstart name changes and fixes

--- web/kickstarter_app/models.py
+++ web/kickstarter_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=1024)
+    category = models.CharField(max_length=1024)
+    main_category = models.CharField(max_length=1024)
+    currency = models.CharField(max_length=1024)
+    deadline = models.CharField(max_length=1024)
+    goal = models.FloatField()
+    launched = models.CharField(max_length=1024)
+    pledged = models.FloatField()
+    state = models.CharField(max_length=1024)
+    backers = models.CharField(max_length=1024)
+    country = models.CharField(max_length=1024)
+    usd_pledged = models.FloatField()
+    # winery = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return '{}'.format(self.name)
--- web/load_db.py
+++ web/load_db.py
@@ -5,22 +5,25 @@ import pandas as pd
 import os
 
 
-csv_data = pd.read_csv('./assets/winemag-data-130k-v2.csv')
+csv_data = pd.read_csv('./assets/ks-projects-201612.csv')
 df = pd.DataFrame(csv_data)
 
 # Adjust NaN values in each column, and generally clean data set
-df['id'] = df['Unnamed: 0'] + 1
+df['ID'] = df['Unnamed: 0'] + 1
 del df['Unnamed: 0']
 
-df['price'] = df['price'].fillna(0.0)
-df['designation'] = df['designation'].fillna('unknown')
-df['region_1'] = df['region_1'].fillna('unknown')
-df['region_2'] = df['region_2'].fillna('unknown')
-df['taster_twitter_handle'] = df['taster_twitter_handle'].fillna('unknown')
-df['taster_name'] = df['taster_name'].fillna('unknown')
+df['name'] = df['name'].fillna('unknown')
+df['category'] = df['category'].fillna('unknown')
+df['main_category'] = df['main_category'].fillna('unknown')
+df['currency'] = df['currency'].fillna('unknown')
+df['deadline'] = df['deadline'].fillna('unknown')
+df['goal'] = df['goal'].fillna('unknown')
+df['launched'] = df['launched'].fillna('unknown')
+df['pledged'] = df['pledged'].fillna(0.0)
+df['state'] = df['state'].fillna('unknown')
+df['backers'] = df['backers'].fillna('unknown')
 df['country'] = df['country'].fillna('unknown')
-df['province'] = df['province'].fillna('unknown')
-df['variety'] = df['variety'].fillna('unknown')
+df['usd pledged'] = df['usd pledged'].fillna('unknown')
 
 
 db_protocol = 'postgresql'
@@ -34,4 +37,4 @@ engine = create_engine('{}://{}:{}@{}:5432/{}'.format(
     db_protocol, db_user, db_password, db_host, db_name
 ))
 
-df.to_sql("reviews_review", engine, if_exists='append', index=False)
+df.to_sql("kickstarter_projects", engine, if_exists='append', index=False)
