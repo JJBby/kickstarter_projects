2a9966ab4890149f2b7a507f21d17ce55df0fab6
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-09-24 14:03:29 -0700

    initializing repo

---
+++ web/kickstarter_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import Review
+
+
+def review_list_view(request):
+    reviews = get_list_or_404(Review)
+    context = {
+        'reviews': reviews
+    }
+
+    return render(request, 'reviews/review_list.html', context)
+
+
+def review_detail_view(request, pk=None):
+    review = get_object_or_404(Review, pk)
+    context = {
+        'review': review
+    }
+
+    return render(request, 'reviews/review_detail.html', context)
